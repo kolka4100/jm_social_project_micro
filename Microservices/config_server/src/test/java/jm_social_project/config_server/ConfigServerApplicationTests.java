339592de01e24a4bb0300b5047bf869d0bb7045a
6opuc-dev <75522440+6opuc-dev@users.noreply.github.com>
2021-06-23 20:15:38 +0300

    Initial commit

---
+++ Microservices/config_server/src/test/java/jm_social_project/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package jm_social_project.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
