87d761fca15f2c76a2c357007dd8b20bd166cb42
ASUTP1300 <denic1300@gmail.com>
2021-06-30 10:07:53 +0300

    server + email_service

---
+++ Microservices/email-service/src/main/java/org/javamentor/social/email_service/model/RequestWrapper.java
@@ -0,0 +1,13 @@
+package org.javamentor.social.email_service.model;
+
+import lombok.Data;
+import org.javamentor.social.email_service.model.dto.AccountDto;
+import org.javamentor.social.email_service.model.entity.Profile;
+
+@Data
+public class RequestWrapper {
+
+    private AccountDto accountDto;
+
+    private Profile profile;
+}
--- Microservices/email-service/src/main/java/org/javamentor/social/email_service/rest_controllers/EmailServiceController.java
+++ Microservices/email-service/src/main/java/org/javamentor/social/email_service/rest_controllers/EmailServiceController.java
@@ -1,6 +1,8 @@
 package org.javamentor.social.email_service.rest_controllers;
 
+import org.javamentor.social.email_service.model.dto.AccountDto;
 import org.javamentor.social.email_service.model.dto.MailDto;
+import org.javamentor.social.email_service.model.RequestWrapper;
 import org.javamentor.social.email_service.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +17,6 @@ public class EmailServiceController {
    public EmailServiceController(EmailService emailService) {
        this.emailService = emailService;
    }
-
 
    @PostMapping("/send-greetingnemail")
    public void sendEmail(@RequestBody String email){
@@ -33,19 +34,11 @@ public class EmailServiceController {
    }
 
    @PostMapping("/send-registration-notification")
-   public void sendRegistrationNotification(@RequestBody String email,
-                                            @RequestBody String firstName,
-                                            @RequestBody String lastName){
-       emailService.sendRegistrationNotification(email, firstName, lastName);
+   public void sendRegistrationNotification(@RequestBody RequestWrapper wrapper){
+       emailService.sendRegistrationNotification(wrapper.getAccountDto().getEmail(),
+               wrapper.getProfile().getFirstName(),
+               wrapper.getProfile().getLastName());
    }
-
-    @PostMapping("/send-payment-notification")
-    public void sendPaymentNotification(@RequestBody String email,
-                                        @RequestBody int lastDays,
-                                        @RequestBody String firstName,
-                                        @RequestBody String lastName){
-       emailService.sendPaymentNotification(email, lastDays, firstName, lastName);
-    }
 }
 
 
