6ebe502e5b0e888aef32ee6dd54fd87f7ef5b090
ASUTP1300 <denic1300@gmail.com>
2021-07-02 15:45:55 +0300

    only eureka server

--- Microservices/email_service/src/main/java/org/javamentor/social/email_service/service/EmailService.java
+++
@@ -1,8 +0,0 @@
-package org.javamentor.social.email_service.service;
-
-public interface EmailService {
-    void sendEmail(String to, String subject, String text);
-    void sendGreetingHtmlEmail(String email);
-    void sendRegistrationNotification(String email, String firstName, String lastName);
-    void sendPaymentNotification(String email, int lastDays, String firstName, String lastName);
-}
