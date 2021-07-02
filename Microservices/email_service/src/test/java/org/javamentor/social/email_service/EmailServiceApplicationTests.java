391bd1524e34ca4b6edd2212f7ebfea750c40c37
ASUTP1300 <denic1300@gmail.com>
2021-07-02 15:53:59 +0300

    initial commit

--- Microservices/email_service/src/test/java/org/javamentor/social/email_service/EmailServiceApplicationTests.java
+++ Microservices/email_service/src/test/java/org/javamentor/social/email_service/EmailServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package org.javamentor.social.eureka_server;
+package org.javamentor.social.email_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaServerApplicationTests {
+class EmailServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
