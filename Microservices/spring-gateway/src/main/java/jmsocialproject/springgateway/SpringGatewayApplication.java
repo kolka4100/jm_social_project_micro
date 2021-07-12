cb648754c34e4f1f677157e9ce0f56b741febc28
6opuc-dev <75522440+6opuc-dev@users.noreply.github.com>
2021-07-12 19:46:23 +0300

    added the property and enabled the eureka client

--- Microservices/spring-gateway/src/main/java/jmsocialproject/springgateway/SpringGatewayApplication.java
+++ Microservices/spring-gateway/src/main/java/jmsocialproject/springgateway/SpringGatewayApplication.java
@@ -2,8 +2,10 @@ package jmsocialproject.springgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringGatewayApplication {
 
 	public static void main(String[] args) {
