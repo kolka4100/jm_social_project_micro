9a935511640d3f794469232ae3ce1c16f4efab12
omon03 <omon03@gmail.com>
2021-08-06 12:32:55 +0500

    init commit

---
+++ Microservices/notification_service/src/main/java/org/javamentor/social/notification_service/NotificationServiceApplication.java
@@ -0,0 +1,9 @@
+package org.javamentor.social.notification_service;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NotificationServiceApplication {
+}
