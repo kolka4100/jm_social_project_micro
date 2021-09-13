8ebbf6a054ee0c0934b9f999358c293c22608a4c
Rustam Garaev <garaev.rustam.h@gmail.com>
2021-09-13 20:00:19 +0400

    test ProfileRestController.getProfileByAccountId()

--- Microservices/profile_service/src/test/java/jm_social_project/profile_service/controller/ProfileRestControllerTest.java
+++ Microservices/profile_service/src/test/java/jm_social_project/profile_service/controller/ProfileRestControllerTest.java
@@ -4,19 +4,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@TestPropertySource("/application.properties")
-@Sql(value = {"/test-profile-controller-before.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 public class ProfileRestControllerTest {
 
     @Autowired
@@ -30,13 +27,12 @@ public class ProfileRestControllerTest {
         assertThat(controller).isNotNull();
     }
 
-//    @Test
-//    void getProfileByAccountId() throws Exception {
-//        MockHttpServletRequestBuilder requestBuilder = get("/profiles/2").header("user_id", "1");
-//        this.mockMvc
-//                .perform(requestBuilder)
-//                .andDo(print())
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.firstName").value("Adam"));
-//    }
+    @Test
+    void getProfileByAccountId() throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = get("/profiles/2").header("user_id", "1");
+        this.mockMvc
+                .perform(requestBuilder)
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
 }
