e2d7370dac3dd0cf93f37a8719dccb481ee5771a
Rustam Garaev <garaev.rustam.h@gmail.com>
2021-09-13 20:25:57 +0400

    added spring rest docs for tests

--- Microservices/profile_service/src/test/java/jm_social_project/profile_service/controller/ProfileRestControllerTest.java
+++ Microservices/profile_service/src/test/java/jm_social_project/profile_service/controller/ProfileRestControllerTest.java
@@ -1,17 +1,28 @@
 package jm_social_project.profile_service.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 @SpringBootTest
 @AutoConfigureMockMvc
 public class ProfileRestControllerTest {
@@ -22,6 +33,14 @@ public class ProfileRestControllerTest {
     @Autowired
     private ProfileRestController controller;
 
+    @BeforeEach
+    public void setUp(WebApplicationContext webApplicationContext,
+                      RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(documentationConfiguration(restDocumentation))
+                .build();
+    }
+
     @Test
     public void contextLoads() throws Exception {
         assertThat(controller).isNotNull();
@@ -31,8 +50,9 @@ public class ProfileRestControllerTest {
     void getProfileByAccountId() throws Exception {
         MockHttpServletRequestBuilder requestBuilder = get("/profiles/2").header("user_id", "1");
         this.mockMvc
-                .perform(requestBuilder)
+                .perform(requestBuilder.accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andDo(document("getProfileByAccountId"));
     }
 }
