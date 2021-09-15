85edfb977063a2ff91aefc2d558bec309d775342
Konstantin Kuznetsov <kskwork@yandex.ru>
2021-09-15 14:28:55 +0300

    Add service/book class in consumer and producer Edit ProducerApplication (add RestController), ConsumerApplication (add RestController)

--- Microservices/kafka-docker/consumer/src/main/java/org/javamentor/consumer/ConsumerApplication.java
+++ Microservices/kafka-docker/consumer/src/main/java/org/javamentor/consumer/ConsumerApplication.java
@@ -1,10 +1,26 @@
 package org.javamentor.consumer;
 
+import org.javamentor.consumer.service.Book;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
 
 @SpringBootApplication
+@EnableBinding(Sink.class)
+
 public class ConsumerApplication {
+
+    private Logger logger = LoggerFactory.getLogger(ConsumerApplication.class);
+
+    @StreamListener("input")
+    public void consumeMessage(Book book) {
+        logger.info("Consumer from kafka topic: " + book);
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
